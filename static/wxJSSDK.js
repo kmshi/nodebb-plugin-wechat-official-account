219f0ebeef5c404694f7983216810307cc3035f0
Richard Shi <kmshi_array@yahoo.com>
2015-11-21 00:45:44 +0800

    wechat qq browser is tricky, meta is not always correct,why?

--- static/wxJSSDK.js
+++ static/wxJSSDK.js
@@ -2,73 +2,46 @@
 
 /*globals ajaxify, config, utils, NProgress*/
 
+var jsApiList = ['getNetworkType','openLocation','hideOptionMenu','onMenuShareAppMessage'];
+
+function configureWeChat(path){
+	$.ajax({
+		url: RELATIVE_PATH + '/api/wechatJSConfig?url=' + encodeURIComponent(path),
+		cache: false,
+		success: function(data) {
+			data.debug = false;
+			data.jsApiList = jsApiList;
+			//alert(JSON.stringify(data));
+			wx.config(data);
+			wx.ready(function(){
+				//wx.hideOptionMenu();
+				wx.onMenuShareAppMessage({
+					title:$("meta[name='title']").attr("content"),
+					desc:$("meta[name='description']").attr("content"),
+					link:window.location.href,
+					imgUrl:'',
+					type:'link',
+					dataUrl:''
+				});
+			});
+			wx.error(function(err){
+				alert(JSON.stringify(err));
+			});
+		},
+		error: function(data, textStatus) {
+		}
+	});
+
+}
+
 $(document).ready(function() {
 	var ua = navigator.userAgent.toLowerCase();
 	var isWeChat = (ua.match(/micromessenger/i) != null);
-	var jsApiList = ['getNetworkType','openLocation','hideOptionMenu','onMenuShareTimeline','onMenuShareAppMessage'];
+	var origUrl = window.location.href.split('#')[0];
 	if (isWeChat){
 		$(window).on('action:ajaxify.end', function() {
-			$.ajax({
-				url: RELATIVE_PATH + '/api/wechatJSConfig?url=' + encodeURIComponent(window.location.href.split('#')[0]),
-				cache: false,
-				success: function(data) {
-					//data.debug = true;
-					data.jsApiList = jsApiList;
-					wx.config(data);
-					wx.ready(function(){
-						wx.hideOptionMenu();
-						/*
-						wx.onMenuShareTimeline({
-							title: '', // 分享标题
-							link: '', // 分享链接
-							imgUrl: '', // 分享图标
-							success: function () {
-								// 用户确认分享后执行的回调函数
-							},
-							cancel: function () {
-								// 用户取消分享后执行的回调函数
-							}
-						});
-						wx.onMenuShareAppMessage({
-							title: '', // 分享标题
-							desc: '', // 分享描述
-							link: '', // 分享链接
-							imgUrl: '', // 分享图标
-							type: '', // 分享类型,music、video或link，不填默认为link
-							dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-							success: function () {
-								// 用户确认分享后执行的回调函数
-							},
-							cancel: function () {
-								// 用户取消分享后执行的回调函数
-							}
-						});
-						wx.getNetworkType({
-							success: function (res) {
-								alert(res.networkType);
-							},
-							fail: function (res) {
-								alert(JSON.stringify(res));
-							}
-						});
-
-						wx.openLocation({
-							latitude: 23.099994,
-							longitude: 113.324520,
-							name: 'TIT 创意园',
-							address: '广州市海珠区新港中路 397 号',
-							scale: 14,
-							infoUrl: 'http://weixin.qq.com'
-						});
-						*/
-					});
-					wx.error(function(err){
-						alert(JSON.stringify(err));
-					});
-				},
-				error: function(data, textStatus) {
-				}
-			});
+			//funny,you have to config everytime but with old url when history pushstate changes
+			if (isWeChat) configureWeChat(origUrl);
 		});
 	}else {
 		window.wx = {};
@@ -77,4 +50,26 @@ $(document).ready(function() {
 			window.wx[jsApiList[idx]]=nullFunc;
 		}
 	}
+
+	$("#getNetworkType").on('click',function(){
+		wx.getNetworkType({
+			success: function (res) {
+				alert(res.networkType);
+			},
+			fail: function (res) {
+				alert(JSON.stringify(res));
+			}
+		});
+	});
+
+	$("#openLocation").on('click',function(){
+		wx.openLocation({
+			latitude: 23.099994,
+			longitude: 113.324520,
+			name: 'TIT 创意园',
+			address: '广州市海珠区新港中路 397 号',
+			scale: 14,
+			infoUrl: 'http://weixin.qq.com'
+		});
+	});
 });
